387ca1ec9088869d45b0ad20457aacb055301647
khushboogupta49 <109016082+khushboogupta49@users.noreply.github.com>
2024-02-14 13:26:32 +0000

    LOVE

---
+++ script.js
@@ -0,0 +1,68 @@
+
+function changeContent(){
+
+document.getElementById("heading")
+
+.innerText= "KAISA LAGA MERA MAGIC  BACHA  ";
+
+
+}
+
+
+  function myFunction() {
+    document.getElementById("para")
+    .style.fontFamily = "cursive";
+  }
+
+
+document.getElementById("btn_for")
+
+.addEventListener("click", function (){
+  this.style.backgroundColor="Violet";
+  
+})
+ 
+
+ function changeImage(){
+ document.getElementById("img");
+ img.src ="IMG20230310105520.jpg";
+ }
+
+
+//  $('button').click( function(){
+
+// $(".magic").toggle(1000);
+// $("h2").css("color" , "green");
+
+
+//  });
+
+
+
+
+//  $('button').click( function(){
+
+//   $(".heading").toggle(1000);
+  
+  
+//    });
+  
+
+
+ 
+// function changeImage(){
+//   document.getElementById("img");
+//   style.backgroundColor="red";
+ 
+ 
+ 
+//   }
+
+// const khusu = document.getElementById("img");
+// const suku = document.getElementById("btn_for");
+// btn_for.addEventListener("click", function(){
+// img.src ="IMG-20231112-WA0059.jpg";
+
+
+
+// })
